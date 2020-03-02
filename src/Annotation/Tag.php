5852ff9313e6018bc1ea6477d9682ab186daa9f4
Blues <i@iblues.name>
2020-03-02 16:36:20 +0800

    fix 多tag模式

--- src/Annotation/Tag.php
+++ src/Annotation/Tag.php
@@ -9,7 +9,6 @@ namespace Iblues\AnnotationTestUnit\Annotation;
  * @link https://github.com/iblues/annotation-test-unit
  * @Annotation
  * Class Tag
- * @Target({"ANNOTATION"})
  * @package Iblues\AnnotationTestUnit
  */
 class Tag
